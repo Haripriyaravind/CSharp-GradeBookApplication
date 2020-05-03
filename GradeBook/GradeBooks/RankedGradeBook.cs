82df94628d16b3c5de175e072c157bf169864e6f
Haripriyaravind <sanga.aravind@gmail.com>
2020-05-03 17:22:02 +0530

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
-﻿using GradeBook.Enums;
+﻿using System;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
@@ -8,5 +9,42 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            var numberOfStudents = Students.Count;
+          
+            if (numberOfStudents < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            var numberOfStudentsGreaterThanGivenAverageGrade = 0; 
+            foreach (var student in Students)
+            {
+                if (student.AverageGrade >= averageGrade )
+                {
+                    numberOfStudentsGreaterThanGivenAverageGrade++;
+                }
+            }
+            var percentage = numberOfStudentsGreaterThanGivenAverageGrade * 100 / numberOfStudents;
+            if (percentage <= 20)
+            {
+                return 'A';
+            }
+            if (percentage > 20 && percentage <= 40)
+            {
+                return 'B';
+            }
+            if (percentage > 40 && percentage <= 60)
+            {
+                return 'C';
+            }
+            if (percentage > 60 && percentage <= 80)
+            {
+                return 'D';
+            }
+            return 'F';
+        }
+
     }
 }
