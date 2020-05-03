a8bb798951eb642cc98643ac744017634afd006b
Haripriyaravind <sanga.aravind@gmail.com>
2020-05-03 17:42:46 +0530

    coverriden calculatestatistics
    overridden methods

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -46,5 +46,28 @@ namespace GradeBook.GradeBooks
             return 'F';
         }
 
+        public override void CalculateStatistics()
+        {
+            var numberOfStudents = Students.Count;
+            if (numberOfStudents < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly " +
+                    "calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            var numberOfStudents = Students.Count;
+            if (numberOfStudents < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly " +
+                    "calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
